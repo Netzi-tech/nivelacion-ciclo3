f624820732522309c5166c7096eb02882b093384
David Diaz <hddiazv@gmail.com>
2022-07-05 17:45:36 -0500

    NC3-4 creación de backend en django

---
+++ hospitalizationApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HospitalizationappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hospitalizationApp'
---
+++ hospitalization_at_home/example_local_settings.py
@@ -0,0 +1,23 @@
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-df-wl%plnjwsnr4_t1mhuga76nsx8v^s6or*jc+ucs(ijfo6cb'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database_name',
+        'USER': 'database_user',
+        'PASSWORD': 'database_password',
+        'HOST': 'database_host',
+        'PORT': 5432,
+        'CHARSET': 'UTF-8',
+        'OPTIONS': {
+            'options': '-c search_path=staging'
+        },
+    }
+}
